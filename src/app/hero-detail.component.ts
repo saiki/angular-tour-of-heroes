74cde3075223587ad1d78731c3808532435ffa2e
unknown <saikiaki@ad.mori-system.co.jp>
2016-11-03 15:05:08 +0900

    user id parameter. add goBack action.

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,12 +1,36 @@
-import { Component, Input } from '@angular/core';
+// Keep the Input import for now, we'll remove it later:
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { HeroService } from './hero.service';
 
 import { Hero } from './hero';
 
 @Component({
   selector: 'my-hero-detail',
-  templateUrl: './hero-detail.component.html'
+  templateUrl: 'hero-detail.component.html',
 })
-export class HeroDetailComponent {
-  @Input()
-  hero: Hero;
+export class HeroDetailComponent implements OnInit {
+
+  @Input() hero: Hero;
+
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) { }
+
+  ngOnInit(): void {
+    this.route.params.forEach((params: Params) => {
+      let id = +params['id'];
+      this.heroService.getHero(id)
+        .then(hero => this.hero = hero);
+    });
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
 }
