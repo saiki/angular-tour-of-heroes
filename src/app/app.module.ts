19cbe3cb6b1c7d9c8f11296e67eb71f032863ef7
unknown <saikiaki@ad.mori-system.co.jp>
2016-11-03 13:20:56 +0900

    split detail component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent,
   ],
   imports: [
     BrowserModule,
