08d5f3b56cb516c475dca4e8eeac306b23292423
unknown <saikiaki@ad.mori-system.co.jp>
2016-11-03 16:35:58 +0900

    implements save button.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -10,6 +10,8 @@ export class HeroService {
 
     private heroesUrl = 'app/heroes';  // URL to web api
 
+    private headers = new Headers({ 'Content-Type': 'application/json' });
+
     constructor(private http: Http) { }
 
     getHeroes(): Promise<Hero[]> {
@@ -22,6 +24,15 @@ export class HeroService {
     getHero(id: number): Promise<Hero> {
         return this.getHeroes()
             .then(heroes => heroes.find(hero => hero.id === id));
+    }
+
+    update(hero: Hero): Promise<Hero> {
+        const url = `${this.heroesUrl}/${hero.id}`;
+        return this.http
+            .put(url, JSON.stringify(hero), { headers: this.headers })
+            .toPromise()
+            .then(() => hero)
+            .catch(this.handleError);
     }
 
     private handleError(error: any): Promise<any> {
