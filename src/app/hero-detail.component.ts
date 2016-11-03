b336413c67a5d1171f6015acf3b1f23b7516033f
unknown <saikiaki@ad.mori-system.co.jp>
2016-11-03 15:42:00 +0900

    add style.

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -10,6 +10,7 @@ import { Hero } from './hero';
 @Component({
   selector: 'my-hero-detail',
   templateUrl: 'hero-detail.component.html',
+  styleUrls: ['hero-detail.component.css'],
 })
 export class HeroDetailComponent implements OnInit {
 
