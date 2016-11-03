b9f4dc7bd92a90a64b88c8128adc4b4182fb7579
unknown <saikiaki@ad.mori-system.co.jp>
2016-11-03 13:52:33 +0900

    add first route.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule }   from '@angular/router';
+
 import { MaterialModule } from '@angular/material';
 
 import { AppComponent }        from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
 import { HeroService }         from './hero.service';
+
+RouterModule.forRoot([
+  {
+    path: 'heroes',
+    component: HeroesComponent
+  }
+]);
 
 @NgModule({
   declarations: [
