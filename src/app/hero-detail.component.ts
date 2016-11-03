f47d380e89b14d13e92de39c4085c583e3df1337
unknown <saikiaki@ad.mori-system.co.jp>
2016-11-03 14:32:29 +0900

    rename selector.

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 import { Hero } from './hero';
 
 @Component({
-  selector: 'hero-detail',
+  selector: 'my-hero-detail',
   templateUrl: './hero-detail.component.html'
 })
 export class HeroDetailComponent {
