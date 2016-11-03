aecb01266749a4d996f05fe9fdb2c32429810224
unknown <saikiaki@ad.mori-system.co.jp>
2016-11-03 14:32:15 +0900

    import RouterModule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule }   from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 import { MaterialModule } from '@angular/material';
 
-import { AppComponent }        from './app.component';
+import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent }     from './heroes.component';
-import { HeroService }         from './hero.service';
-
-RouterModule.forRoot([
-  {
-    path: 'heroes',
-    component: HeroesComponent
-  }
-]);
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
 
 @NgModule({
   declarations: [
@@ -28,11 +21,17 @@ RouterModule.forRoot([
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule.forRoot()
+    MaterialModule.forRoot(),
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      }
+    ]),
   ],
   providers: [
     HeroService
   ],
-  bootstrap: [ AppComponent ]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
