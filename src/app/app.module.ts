bc9bec8be441945b62edd276ae7d626ef33aff9c
unknown <saikiaki@ad.mori-system.co.jp>
2016-11-03 13:50:20 +0900

    add new AppComponent.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 
-import { AppComponent } from './app.component';
+import { AppComponent }        from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent }     from './heroes.component';
+import { HeroService }         from './hero.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroDetailComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
@@ -18,7 +21,9 @@ import { HeroDetailComponent } from './hero-detail.component';
     HttpModule,
     MaterialModule.forRoot()
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    HeroService
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
